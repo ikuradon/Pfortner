4b03f71157f7e01c2acf7a76f0441627878d84b3
ikuradon <hawaiianphoto@geekhost.net>
2023-11-17 00:00:49 +0900

    deno-types

--- deps.ts
+++ deps.ts
@@ -1,5 +1,6 @@
 export { Hono } from "https://deno.land/x/hono@v3.10.0/mod.ts";
 export * as nostrTools from "npm:nostr-tools@1.17.0";
 export * as dotenv from "https://deno.land/std@0.206.0/dotenv/mod.ts";
+// @deno-types="npm:@types/ws@8.5.9"
 import WebSocket from "npm:ws@8.14.2";
 export const nWebSocket = WebSocket;
