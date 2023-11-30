aca34eed33bc57ccc41f8b55cc835ea4907bfd67
ikuradon <hawaiianphoto@geekhost.net>
2023-12-01 00:00:39 +0900

    ⁽⁽🌲₎₎

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,1 @@
 export * as nostrTools from 'npm:nostr-tools@1.17.0';
-// @deno-types="npm:@types/ws@8.5.9"
-import WebSocket from 'npm:ws@8.14.2';
-export const ws = WebSocket;
-export type ws = WebSocket;
-export type wsClientOptions = WebSocket.ClientOptions;
