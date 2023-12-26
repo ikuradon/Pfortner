7c60af5d0daf37dd103315f458b2b493b8027f76
ikuradon <hawaiianphoto@geekhost.net>
2023-12-27 07:55:31 +0900

    Update packages

--- scripts/deps.ts
+++ scripts/deps.ts
@@ -1,2 +1,2 @@
-export * as nostrTools from 'npm:nostr-tools@2.1.0';
+export * as nostrTools from 'npm:nostr-tools@2.1.1';
 export * as dotenv from 'https://deno.land/std@0.210.0/dotenv/mod.ts';
--- src/deps.ts
+++ src/deps.ts
@@ -1,1 +1,1 @@
-export * as nostrTools from 'npm:nostr-tools@2.1.0';
+export * as nostrTools from 'npm:nostr-tools@2.1.1';
